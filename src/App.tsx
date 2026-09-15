0a6c14901bfa294e7d2d1b3cd1456e6e4a114509
Junghoon Yoon <juhuyoon@yahoo.com>
2026-09-14 21:51:24 -0400

    pushing up baseline pages, included sample github actions build, test push for GitHub pages

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import Landing from './Components/landingPage/Landing.tsx'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <p>
+            <Landing />
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/landingPage/Landing.tsx
@@ -0,0 +1,7 @@
+export default function Landing() {
+    return (
+        <div className="text-3xl font-bold underline">
+            This is the initial Landing page of the wedding invitation!
+        </div>
+    )
+}
